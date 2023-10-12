3adf776da9964c0a117c682888c6493e895af4be
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2023-10-12 11:54:05 +0800

    [Update -- GA]

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
+import Script from 'next/script';
 export default function MyApp({ Component, pageProps }) {
 
     return <>
         <Head><link rel="icon" href="/favicon.ico" /></Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-142989569-1"></Script>
+        <Script>
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-142989569-1');
+            `}
+        </Script>
         <Component {...pageProps} />
+
     </>
 }
